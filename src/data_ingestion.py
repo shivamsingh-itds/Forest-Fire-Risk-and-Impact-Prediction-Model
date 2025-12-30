d97dd16b676f48cb3ad9e1d19eda20e05000aa65
shivamsingh-itds <shivam.itds@gmail.com>
2025-12-30 22:43:59 +0530

    Data Ingestion

---
+++ src/data_ingestion.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from pathlib import Path
+
+def load_data():
+    
+    base_dir = Path(__file__).resolve().parent.parent
+    data_path = base_dir / "data" / "raw" / "forestfires.csv"
+
+    df = pd.read_csv(data_path)
+    return df
